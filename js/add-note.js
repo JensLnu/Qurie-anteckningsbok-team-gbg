f6a3ec3f166cb3d9e8ef9a9fea9fdc8637db9b2c
Emil <emil.winberg1@gmail.com>
2024-01-15 16:39:31 +0100

    Add texarea on click, Save to local storage, minor css

---
+++ js/add-note.js
@@ -0,0 +1,12 @@
+const addBtn = document.querySelector('.add-btn');
+const textArea = document.getElementById('text-area');
+
+addBtn.addEventListener('click', () => {
+    textArea.innerHTML = '<textarea name="title" id="title"></textarea>';
+    const note = document.getElementById('title');
+    note.addEventListener('input', () => {
+        localStorage.setItem('text', note.value);
+    })
+    note.focus();
+});
+
