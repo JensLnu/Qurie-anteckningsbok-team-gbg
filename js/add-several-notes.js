5ff32ae919381a6ad6fa9e3054a61e7e392a91af
Tova Lundgren <tova.lundgren@chasacademy.se>
2024-01-16 10:07:29 +0100

    add comments to plan our coding

---
+++ js/add-several-notes.js
@@ -0,0 +1,14 @@
+/*--- Som användare vill jag kunna skapa flera olika anteckningar ---*/
+
+// Hämta add-btn och notes från HTML
+
+// Event listener för add-btn
+    // Skapa ny antecknings container (t.ex note)
+    // Skapa ny textarea för anteckningen
+    // Skapa event listener för input eventet och spara till localStorage
+    // Appenda textarea till note container
+    // Skapa en delete knapp för anteckningen ???
+    // Skapa event listener för delete knappen och ta bort från localStorage ???
+    // Appenda deleteknapp till note container
+    // Appenda note container till notes container
+    // Focus på den nya anteckningen ???
