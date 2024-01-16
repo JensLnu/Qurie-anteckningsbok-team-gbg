ce1354e326dd8f9f74683ee5b2afb67e8b3af495
Emil <emil.winberg1@gmail.com>
2024-01-16 15:11:24 +0100

    Add functionality for mobile add button

--- js/add-note.js
+++ js/add-note.js
@@ -1,12 +1,16 @@
 const addBtn = document.querySelector('.add-btn');
+const addBtnMobile = document.querySelector('.add');
 const textArea = document.getElementById('text-area');
 
 
-addBtn.addEventListener('click', () => {
+addBtn.addEventListener('click', addNote);
+addBtnMobile.addEventListener('click', addNote);
+
+function addNote(){
     textArea.innerHTML = '<textarea name="title" id="title"></textarea>';
     const note = document.getElementById('title');
     note.addEventListener('input', () => {
         localStorage.setItem('text', note.value);
     })
     note.focus();
-});
+}
