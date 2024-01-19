442d516927eccfa16101a32edb6bce3d18b25970
Jens Bengtsson <jb225je@student.lnu.se>
2024-01-19 09:05:11 +0100

    join swap-notes with rest of the code

--- js/swap-note.js
+++ js/swap-note.js
@@ -2,26 +2,24 @@
 6. (5.3 points) Som användare vill jag kunna bläddra bland mina olika anteckningar på ett smidigt sätt -- Adam och Jens
 */
 
-//global refrens
-const allNotes = document.querySelectorAll('.notes'); // array med alla div-element (notes)
-
-chooseNote();
+document.addEventListener('DOMContentLoaded', chooseNote);
 
 // makes all notes clickable
 function chooseNote() {
+    const allNotes = document.querySelectorAll('.notes'); // array med alla div-element (notes)
     allNotes.forEach(note => {
         note.addEventListener('click', (e) => {
-            displayNote(e);
+            displayNote(e, allNotes);
         });
     })
 }
 
 // display clicked note
-function displayNote(e) {
+function displayNote(e, allNotes) {
     let selectedNote = e.currentTarget;
     allNotes.forEach(note => {
         note.classList.remove('displayed-note');
     });
     selectedNote.classList.add('displayed-note');
-    textArea.textContent = selectedNote.textContent;
+    textArea.textContent = selectedNote.innertext;
 }
