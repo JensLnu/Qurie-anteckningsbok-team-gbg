c9e67a9c4c49ec0072311802a291f50e50f2bb51
Jens Bengtsson <jb225je@student.lnu.se>
2024-01-19 11:16:24 +0100

    now swap-note.js works with rest of the code

--- js/swap-note.js
+++ js/swap-note.js
@@ -1,5 +1,5 @@
 /*
-6. (5.3 points) Som användare vill jag kunna bläddra bland mina olika anteckningar på ett smidigt sätt -- Adam och Jens
+storie 6. (5.3 points) Som användare vill jag kunna bläddra bland mina olika anteckningar på ett smidigt sätt -- Adam och Jens
 */
 
 document.addEventListener('DOMContentLoaded', chooseNote);
@@ -15,14 +15,13 @@ function chooseNote() {
     })
 }
 
-// display clicked note
+// displays clicked note
 function displayNote(e, allNotes) {
-    console.log('displayNote')
     let selectedNote = e.currentTarget;
     allNotes.forEach(note => {
         note.classList.remove('displayed-note');
     });
     selectedNote.classList.add('displayed-note');
     const header = selectedNote.firstElementChild.textContent;
-    textArea.innerHTML = `<h2>${header}</h2><p>${localStorage.getItem(header)}</p>`;
+    textArea.innerHTML = `<h2>${header}</h2><p>${localStorage.getItem(header.charAt(header.length - 1))}</p>`;
 }
