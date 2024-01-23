e1ad2203372505726575a734bcde1f0241510eb3
Emil <emil.winberg1@gmail.com>
2024-01-23 16:14:01 +0100

    Fix bug where value is saved instead of innerHTML

--- js/edit.js
+++ js/edit.js
@@ -2,8 +2,13 @@
 
 const btnOptions = document.querySelector('.btn-options');
 const notesBody = document.querySelector('.notes__body');
+const boldBtn = document.getElementById('bold');
 
 btnOptions.addEventListener('click', changeFontStyle);
+boldBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.getSelection();
+})
 
 function changeFontStyle(e) {
     if (e.target.id === "bold" || e.target.id === "italic" || e.target.id === "header" || e.target.id === "createbulletList" || e.target.id === "createNumberedList") {
@@ -12,3 +17,11 @@ function changeFontStyle(e) {
     }
 }
 
+function replaceSelected(selectedText) {
+    const selected = window.getSelection();
+    if(selected.rangeCount) {
+        const range = selected.getRangeAt(0);
+        range.deleteContents();
+        range.insertNode(`<strong>${selectedText}</strong>`);
+    }
+}
