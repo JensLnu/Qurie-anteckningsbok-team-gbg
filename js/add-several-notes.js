5b6b422bc1c3e6dcb71c08f927104f2624cc2f7d
Emil <emil.winberg1@gmail.com>
2024-01-23 15:36:55 +0100

    Change textarea for editable div in severalnotes.js

--- js/add-several-notes.js
+++ js/add-several-notes.js
@@ -77,8 +77,9 @@ displayAllNotes();
 
 addBtnSeveral.addEventListener('click', () => {
     const mainTextArea = document.getElementById('text-area');
-    const noteTextarea = document.createElement('textarea');
-    noteTextarea.classList.add('note-textarea');
+    const noteTextarea = document.createElement('div');
+    noteTextarea.className = "note-textarea";
+    noteTextarea.setAttribute("contenteditable", "true");
 
     // Unikt ID för varje anteckning
     noteCounter++
@@ -89,7 +90,7 @@ addBtnSeveral.addEventListener('click', () => {
     mainTextArea.textContent = '';
     mainTextArea.appendChild(noteTextarea);
 
-    // Lägg till den nya anteckningen i DOM
+    // Lägg till den nya anteckningen i DOM (sidebar)
     createNotesContainer(noteId);
     // Kalla på chooseNote för att kunna bläddra bland anteckningarna
     chooseNote();
