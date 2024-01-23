79a430ff4950a0a20c448d8f44c05a33df4f5d57
Toleen <toleen.alrifai@chasacademy.se>
2024-01-23 10:35:24 +0100

    "add new changes to edit files"

--- js/edit.js
+++ js/edit.js
@@ -1,29 +1,14 @@
 //Som användare vill jag kunna skapa rubriker, punktlistor, numrerade listor samt göra text kursiv eller fetstil
 
-const btnOptions= document.querySelector('.btn-options');
-btnOptions. addEventListener('click', changeFontStyle);
+const btnOptions = document.querySelector('.btn-options');
+const notesBody = document.querySelector('.notes__body');
 
+btnOptions.addEventListener('click', changeFontStyle);
 
 function changeFontStyle(e) {
-   const text = document.querySelector('.notesPreview') ;
-    if(e.target.id === "bold") {
+    if (e.target.id === "bold" || e.target.id === "italic" || e.target.id === "header" || e.target.id === "createbulletList" || e.target.id === "createNumberedList") {
         e.target.classList.toggle('active');
-        text.classList.toggle('bold');
-    }  
-    if(e.target.id === "italic") {
-        e.target.classList.toggle('active');
-        text.classList.toggle('italic');
-    }  
-    if(e.target.id === "header") {
-        e.target.classList.toggle('active');
-        text.classList.toggle('2em');
-    }  
-    if(e.target.id === "createbulletList") {
-        e.target.classList.toggle('active');
-        text.classList.toggle('disc');
-    }  
-    if(e.target.id === "createNumberedList") {
-        e.target.classList.toggle('active');
-        text.classList.toggle('decimal');
+        notesBody.classList.toggle(e.target.id);
     }
 }
+
