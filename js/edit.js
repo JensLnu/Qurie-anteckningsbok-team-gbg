06f7bf993eefc12b362264c43b4edd2bc1a2b2dd
Emil <emil.winberg1@gmail.com>
2024-01-23 16:36:19 +0100

    Change approach for edit text

--- js/edit.js
+++ js/edit.js
@@ -1,27 +1,14 @@
 //Som användare vill jag kunna skapa rubriker, punktlistor, numrerade listor samt göra text kursiv eller fetstil
+const boldBtn = document.getElementById('bold'); //<strong></strong>
+const italicBtn = document.getElementById('italic'); //<em> </em>
+const heading1Btn = document.getElementById('heading-1'); //<h1></h1>
+const heading2Btn = document.getElementById('heading-2');//<h2></h2>
+const heading3Btn = document.getElementById('heading-3');//<h3></h3>
+const ulBtn = document.getElementById('unordered-list'); //<ul><li></li></ul>
+const olBtn = document.getElementById('ordered-list'); //<ol><li></li></ol>
+const regularBtn = document.getElementById('regular'); // <p></p>
 
-const btnOptions = document.querySelector('.btn-options');
-const notesBody = document.querySelector('.notes__body');
-const boldBtn = document.getElementById('bold');
+// Kunna markera text och hitta igen det i HTML
+// Omringa den markerade texten av relevanta taggar
+// Använd e.preventDefault() för att förhindra att markeringen försvinner
 
-btnOptions.addEventListener('click', changeFontStyle);
-boldBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.getSelection();
-})
-
-function changeFontStyle(e) {
-    if (e.target.id === "bold" || e.target.id === "italic" || e.target.id === "header" || e.target.id === "createbulletList" || e.target.id === "createNumberedList") {
-        e.target.classList.toggle('active');
-        notesBody.classList.toggle(e.target.id);
-    }
-}
-
-function replaceSelected(selectedText) {
-    const selected = window.getSelection();
-    if(selected.rangeCount) {
-        const range = selected.getRangeAt(0);
-        range.deleteContents();
-        range.insertNode(`<strong>${selectedText}</strong>`);
-    }
-}
