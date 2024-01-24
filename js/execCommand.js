335b8d3b39a4e96fba1e71033d4073e9d6abd024
Toleen <toleen.alrifai@chasacademy.se>
2024-01-24 17:02:55 +0100

    add new css, addexexCommand.js for the btns, add new html

---
+++ js/execCommand.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const headings = document.querySelectorAll('.headings')
+    const toolbarButtons = document.querySelectorAll('.toolbar-button')
+    
+    function modifyText(command, defaultUi, value) {
+        document.execCommand(command, defaultUi, value);
+    }
+    
+    toolbarButtons.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            btn.classList.toggle('active');
+            const textarea = document.querySelector('.note-textarea')
+            modifyText(btn.id, false, null);
+            textarea.focus();
+        });
+    })
+    
+    headings.forEach((btn) => {
+        btn.addEventListener('change', () => {
+            const textarea = document.querySelector('.note-textarea')
+            modifyText(btn.id, false, btn.value)
+            textarea.focus();
+        })
+    })
+})
