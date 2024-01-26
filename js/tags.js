42dfe4276c0a9a2435524aa5db32c00f37183fa9
Tova Lundgren <tova.lundgren@chasacademy.se>
2024-01-26 11:28:16 +0100

    add pseudo code

---
+++ js/tags.js
@@ -0,0 +1,10 @@
+/*--- Som användare vill jag kunna lägga taggar på mina anteckningar och kunna plocka fram alla anteckningar med en viss tag ---*/
+
+/* Som användare vill jag kunna:
+- via en knapp # bredvid delete-btn kunna lägga till taggar för en anteckning.
+- se mina taggar under titeln i sidebar när noten är aktiv.
+- söka efter specifika taggar via search i navigation
+    - se enbart de notes med sökt tagg i sidebar (utan att taggarna visas under varje note)
+    - text-area är tom efter sökt tagg, tills man väljer en note
+- visa och ta bort taggar i varje note
+*/
