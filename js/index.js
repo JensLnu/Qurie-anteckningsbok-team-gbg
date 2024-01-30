4621fcbd1aad460f63a6ab59201c3e75c6c6f869
Emil <emil.winberg1@gmail.com>
2024-01-30 10:40:22 +0100

    And again

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 document.getElementById('logo').addEventListener('click', () => {
-    gtag('event', 'home-button click', {
+    gtag('event', 'G-7BZMN42MS3', {
         'event-category': 'click',
         'event-label': 'Någon stackare har försökt ta sig tillbaka till startsidan på det här viset'
     })
