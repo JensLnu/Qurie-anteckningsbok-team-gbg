102209a23a0487abd3ec559b3d84c31d228ab2f1
Emil <emil.winberg1@gmail.com>
2024-01-30 10:36:53 +0100

    Try again with gtag

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 document.getElementById('logo').addEventListener('click', () => {
-    gtag('click', 'home-button', {
+    gtag('event', 'home-button click', {
         'event-category': 'click',
         'event-label': 'Någon stackare har försökt ta sig tillbaka till startsidan på det här viset'
     })
