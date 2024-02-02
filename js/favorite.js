1f3d6a6b12fd4cded9cc62d241ef5abc45ba8d5a
Toleen <toleen.alrifai@chasacademy.se>
2024-02-03 00:36:39 +0100

    add new style to stars-icon

--- js/favorite.js
+++ js/favorite.js
@@ -7,24 +7,38 @@ function toggleFavorite() {
 
 function findFavorites() {
     let savedNote;
-    let noteContainer = document.querySelector('.saved-notes');    
+    let noteContainer = document.querySelector('.saved-notes');
+    let starsIcon = document.getElementById('stars-icon');
+
+    starsIcon.classList.toggle('active');
+
     noteContainer.innerHTML = '';
     let localKey;
 
     for (let i = 0; i < localStorage.length; i++) {
         localKey = localStorage.key(i);
-        savedNote = JSON.parse(localStorage.getItem(localKey));
-        
-        if (savedNote.favorite) { //if-satsen körs bara för de som är true             
-            createNotesContainer(savedNote.noteId);
-        }         
+
+        try {
+            savedNote = JSON.parse(localStorage.getItem(localKey));
+
+            if (starsIcon.classList.contains('active')) {
+                if (savedNote.favorite) {
+                    createNotesContainer(savedNote.noteId);
+                }
+            } else {
+                createNotesContainer(savedNote.noteId);
+            }
+        } catch (error) {
+            console.error(`Error parsing JSON for localStorage key ${localKey}: ${error}`);
+        }
     }
+
     chooseNote();    
 }
 
 // Event listeners
 document.querySelector('.favorite-button').addEventListener('click', toggleFavorite);
-
 document.getElementById('stars-icon').addEventListener('click', findFavorites);
+
 
 
