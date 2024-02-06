0cf44a94e104c82f8bda78f376d6c058389af753
Jens Bengtsson <jb225je@student.lnu.se>
2024-02-06 11:15:15 +0100

    moved code form main.js to index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+import { chooseNoteTemplate } from "./moduls/note-template.js"
+
 document.getElementById('logo-container').addEventListener('click', () => {
     gtag('event', 'home-btn-click', {
         'event-category': 'click',
@@ -5,3 +7,7 @@ document.getElementById('logo-container').addEventListener('click', () => {
         'event-author' : 'Emil :-)'
     })
 })
+
+// förslag på att ha en main.js fil som läser in standard funktionalitet?
+
+noteTemplateBtn.addEventListener('click', chooseNoteTemplate); // aktiverar knappen för att välja antecknings mall
