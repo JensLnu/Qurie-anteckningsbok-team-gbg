3bfe7c81cadd382d03ad6202943c351e112166cf
Nyat Ghirmazion <nyat.ghirmazion@chasacademy.se>
2024-02-09 10:02:03 +0100

    push

--- js/index.js
+++ js/index.js
@@ -1,18 +1,36 @@
 import { createFunctionalityNoteTemplate } from "./moduls/note-template.js"
 
-document.getElementById('logo-container').addEventListener('click', () => {
-    console.log('connection');
-    const textArea = document.getElementById('text-area');
-    textArea.innerHTML = '';
+(async function getFirstVisit() {
+    let response = await fetch('./json/firstVisit.json');
+    if (response.ok) {
+        response = await response.json();
+        displayFirstMessage(response);
 
-    textArea.innerHTML = 'STARTSIDA'
+document.getElementById('logo-container').addEventListener('click', () => {
+    const textArea = document.getElementById('text-area');
+    textArea.innerHTML = `
+        <div id="first-div" spellcheck = 'false' contenteditable = 'false' >
+            <h1 id = "first-rubrik">Välkommen till Qurie</h1>
+            <p>${response.paraText}</p>
+            <div class = "info-div"><h3 class = "header-info">Navigera</h3><p class = "p-info">${response.search}</p></div>
+            <div class = "info-div"><h3 class = "header-info">Favorisera</h3><p class = "p-info">${response.favorite}</p></div>
+            <div class = "info-div"><h3 class = "header-info">Analysera</h3><p class = "p-info">${response.analyse}</p></div>
+            <div class = "info-div"><h3 class = "header-info">Konfigurera</h3><p class = "p-info">${response.settings}</p></div>        
+        </div > `;
 
     gtag('event', 'home-btn-click', {
         'event-category': 'click',
         'event-label': 'Någon stackare har försökt ta sig tillbaka till startsidan på det här viset',
         'event-author' : 'Emil :-)'
     })
+    
 })
+    } else {
+        console.log('Error:', response.status);
+    }
+})();
+
+
 
 window.addEventListener('DOMContentLoaded', () => {
     createFunctionalityNoteTemplate(); // aktiverar knappen för att välja antecknings mall
