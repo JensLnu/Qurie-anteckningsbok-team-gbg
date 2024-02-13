3b5305e3d1189731ff39056027053fac842f7321
Oscar Donaldson <oscar@donaldson.se>
2024-02-13 11:43:16 +0100

    merge

---
+++ js/main.js
@@ -0,0 +1,5 @@
+import { chooseNoteTemplate } from "./moduls/note-template.js"
+
+// förslag på att ha en main.js fil som läser in standard funktionalitet?
+
+noteTemplateBtn.addEventListener('click', chooseNoteTemplate); // aktiverar knappen för att välja antecknings mall
