dbf7fcef5124a975bc9edbb22e7cbd7d929e2e28
Tova Lundgren <tova.lundgren@chasacademy.se>
2024-02-13 10:58:44 +0100

    add pseudo

---
+++ js/convert-to-markdown.js
@@ -0,0 +1,8 @@
+/* ----- Som användare vill jag kunna exportera en anteckning som markdown -----*/
+
+/*
+[] En knapp för att exportera till markdown
+[] Ett fönster med markdownversionen eller konvertera direkt i text-area:
+    []  Om konverteringen sker i text-area: knapp för att byta tillbaka till default
+    [] Spara i lS
+*/
