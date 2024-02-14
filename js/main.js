e87c9128b0ed5620334e31ffed9e4b29ba09e6d0
Jens Bengtsson <jb225je@student.lnu.se>
2024-02-14 10:04:01 +0100

    removed file & comments

--- js/main.js
+++
@@ -1,5 +0,0 @@
-import { chooseNoteTemplate } from "./moduls/note-template.js"
-
-// förslag på att ha en main.js fil som läser in standard funktionalitet?
-
-noteTemplateBtn.addEventListener('click', chooseNoteTemplate); // aktiverar knappen för att välja antecknings mall
