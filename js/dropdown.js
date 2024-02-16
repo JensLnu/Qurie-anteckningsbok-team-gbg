db923f8364a461098a0ca321e122eb21a32bd213
Nyat Ghirmazion <nyat.ghirmazion@chasacademy.se>
2024-02-16 11:29:11 +0100

    fixed so old notes show in mobile versions

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,6 +1,8 @@
 displayBtn.addEventListener('click', toggleDisplay);
 dropdownMenu.addEventListener('click', toggleDisplay);
 
+const showNotes = document.querySelector('.show-notes');
+const sidebar = document.getElementById('sidebar');
 // Eventlistener för att visa mobil-dropdown meny
 // Kolla om menyn är aktiv
 // Eventlistener på bodyn behövde timeout för att fungera, gör det möjligt att klicka bort menyn när man klickar utanför. (Hade gått att lösa med e.client och  dropdownens dimensioner)
@@ -20,3 +22,13 @@ function toggleDisplay() {
         }, 0.1);
     }
 }
+showNotes.addEventListener('click', function () {
+    rootColors.style.setProperty('--toggle-sidebar', 'block');
+    sidebar.style.zIndex = 999;
+});
+
+sidebar.addEventListener('focusout', function () {
+    if (window.innerWidth < 700) {
+    rootColors.style.setProperty('--toggle-sidebar', 'none');
+    }
+})
