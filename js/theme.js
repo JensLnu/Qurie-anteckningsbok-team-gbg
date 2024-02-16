60f2fc94f231b0e8c3b38e030555d96b2fd754f6
David Heidari <davidheidari2@gmail.com>
2024-02-16 17:08:37 +0100

    delete a comment in theme.js

--- js/theme.js
+++ js/theme.js
@@ -3,10 +3,10 @@ document.addEventListener('DOMContentLoaded', setTheme);
 
 // Öppnar modalen
 // openThemeModal.addEventListener("click", () => dialog.showModal());
-
+// Davids g-tag
 openThemeModal.addEventListener("click", () => {
-  dialog.showModal(); // Anropa först showModal() utan extra argument
-  gtag('event', 'new_theme', { // Sedan anropa gtag för att spåra händelsen
+  dialog.showModal(); 
+  gtag('event', 'new_theme', { // Anropar gtag för att spåra händelsen
       'event_category': 'theme_setting',
       'event_label': 'change the theme',
       'value': 'David' 
@@ -75,7 +75,7 @@ function neonTheme() {
     rootColors.style.setProperty("--displayed-note-color", "rgb(52, 52, 52)")
     rootColors.style.setProperty("--edit-buttons-primary", "rgb(35,35,35)")
     rootColors.style.setProperty("--edit-buttons-secondary", "rgb(85,85,85)")
-    rootColors.style.setProperty("--edit-buttons-hover", "#F5F4F5")
+    rootColors.style.setProperty("--edit-buttons-hover", "rgb(255,0,110)")
     rootColors.style.setProperty("--first-visit-primary", "rgb(34,84,110)")
     rootColors.style.setProperty("--note-bg-color", "rgb(35,35,35)")
     rootColors.style.setProperty("--neon-text-shadow", "10 0 10px #fff")
@@ -108,8 +108,7 @@ function darkmodeTheme() {
     rootColors.style.setProperty("--neon-text-shadow", "10 0 10px #fff")
     rootColors.style.setProperty("--bgColor", "rgb(35,35,35)")
     rootColors.style.setProperty("--notes-text-color", "white")
-    rootColors.style.setProperty("--print-icon", "white")
-  
+
     localStorage.setItem('theme', 'dark');
 }
 
@@ -136,7 +135,6 @@ function quireTheme() {
    rootColors.style.setProperty("--neon-text-shadow", "none"); 
    rootColors.style.setProperty("--bgColor", "rgb(237, 247, 255)"); 
    rootColors.style.setProperty("--notes-text-color", "rgb(30, 136, 229)");
-  rootColors.style.setProperty("--print-icon", "rgb(26, 35, 126))");
 
    localStorage.setItem('theme', 'quire');
  
@@ -178,7 +176,3 @@ dialog.addEventListener("click", e => {
         dialog.close();
     }
 });
-  
-
-  
-
